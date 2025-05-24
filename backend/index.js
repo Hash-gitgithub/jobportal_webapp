f0ea7f6b0e3163e4e1b7a14940c8e195c7b39b51
Hashir <hairymartin03@gmail.com>
2025-05-24 20:24:34 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://jobportalnew-alpha.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true
 }
 
